2df8f4372b32e4a0f9a29a0e453873a8fbfcd406
Florian <fridel.florian@hotmail.fr>
2020-03-18 10:02:38 +0100

    First commit

---
+++ Characters.js
@@ -0,0 +1,355 @@
+const characters = 
+    {
+        berserk: {
+            female : {
+                dolores: {
+                    life: 100,
+                    mana: 100,
+                    atk: 10,
+                    def: 5,
+                    str: 10,
+                    dex: 3,
+                    int: 0
+                },
+                clare: {
+                    life: 100,
+                    mana: 100,
+                    atk: 10,
+                    def: 5,
+                    str: 10,
+                    dex: 3,
+                    int: 10
+                },
+                ripley : {
+                    life: 100,
+                    mana: 100,
+                    atk: 8,
+                    def: 5,
+                    str: 8,
+                    dex: 5,
+                    int: 10
+                },
+                garona: {
+                    life: 100,
+                    mana: 100,
+                    atk: 10,
+                    def: 7,
+                    str: 7,
+                    dex: 5,
+                    int: 5
+                }
+            },
+            male : {
+                guts: {
+                    life: 100,
+                    mana: 100,
+                    atk: 99,
+                    def: 99,
+                    str: 99,
+                    dex: 99,
+                    int: 99
+                },
+                conan: {
+                    life: 100,
+                    mana: 100,
+                    atk: 10,
+                    def: 5,
+                    str: 10,
+                    dex: 3,
+                    int: 3
+                },
+                olaf: {
+                    life: 100,
+                    mana: 100,
+                    atk: 8,
+                    def: 8,
+                    str: 8,
+                    dex: 3,
+                    int: 4
+                },
+                thorgal: {
+                    life: 100,
+                    mana: 100,
+                    atk: 10,
+                    def: 10,
+                    str: 10,
+                    dex: 10,
+                    int: 10
+                }
+            }
+        },
+        ranger: {
+            female: {
+               tauriel: {
+                life: 100,
+                mana: 100,
+                atk: 10,
+                def: 3,
+                str: 5,
+                dex: 10,
+                int: 10
+               } ,
+               sylvanas: {
+                life: 100,
+                mana: 100,
+                atk: 8,
+                def: 5,
+                str: 5,
+                dex: 10,
+                int: 10
+               },
+               jane: {
+                life: 100,
+                mana: 100,
+                atk: 5,
+                def: 2,
+                str: 5,
+                dex: 6,
+                int: 3
+               },
+               lara: {
+                life: 100,
+                mana: 100,
+                atk: 6,
+                def: 4,
+                str: 6,
+                dex: 10,
+                int: 10
+               }
+            },
+            male: {
+                robin: {
+                    life: 100,
+                    mana: 100,
+                    atk: 7,
+                    def: 5,
+                    str: 6,
+                    dex: 8,
+                    int: 5
+                },
+                legolas: {
+                    life: 100,
+                    mana: 100,
+                    atk: 8,
+                    def: 5,
+                    str: 6,
+                    dex: 10,
+                    int: 10
+                },
+                rexxar: {
+                    life: 100,
+                    mana: 100,
+                    atk: 8,
+                    def: 5,
+                    str: 7,
+                    dex: 9,
+                    int: 6
+                },
+                tic: {
+                    life: 50,
+                    mana: 0,
+                    atk: 1,
+                    def: 0,
+                    str: 0,
+                    dex: 0,
+                    int: 10
+                }
+            }
+        },
+        rogue: {
+            female: {
+               fujiko: {
+                life: 100,
+                mana: 100,
+                atk: 6,
+                def: 4,
+                str: 5,
+                dex: 10,
+                int: 10
+               },
+               nami: {
+                life: 100,
+                mana: 100,
+                atk: 4,
+                def: 3,
+                str: 4,
+                dex: 6,
+                int: 10
+               },
+               bonnie: {
+                life: 100,
+                mana: 100,
+                atk: 7,
+                def: 4,
+                str: 5,
+                dex: 6,
+                int: 7
+               },
+               tonilia: {
+                life: 100,
+                mana: 100,
+                atk: 7,
+                def: 5,
+                str: 5,
+                dex: 10,
+                int: 6
+               }
+            },
+            male: {
+                arsene: {
+                    life: 100,
+                    mana: 100,
+                    atk: 10,
+                    def: 6,
+                    str: 6,
+                    dex: 10,
+                    int: 10
+                },
+                zidane: {
+                    life: 100,
+                    mana: 100,
+                    atk: 8,
+                    def: 6,
+                    str: 6,
+                    dex: 10,
+                    int: 6
+                },
+                sly: {
+                    life: 100,
+                    mana: 100,
+                    atk: 7,
+                    def: 5,
+                    str: 5,
+                    dex: 8,
+                    int: 6
+                },
+                abu: {
+                    life: 100,
+                    mana: 100,
+                    atk: 1,
+                    def: 1,
+                    str: 1,
+                    dex: 10,
+                    int: 5
+                }
+            }
+        },
+        wizard: {
+            female: {
+                jaina: {
+                    life: 100,
+                    mana: 100,
+                    atk: 8,
+                    def: 4,
+                    str: 4,
+                    dex: 5,
+                    int: 10
+                },
+                galadriel: {
+                    life: 100,
+                    mana: 100,
+                    atk: 7,
+                    def: 4,
+                    str: 5,
+                    dex: 6,
+                    int: 10
+                },
+                morgane: {
+                    life: 100,
+                    mana: 100,
+                    atk: 5,
+                    def: 4,
+                    str: 4,
+                    dex: 5,
+                    int: 9
+                },
+                raziel: {
+                    life: 100,
+                    mana: 100,
+                    atk: 6,
+                    def: 4,
+                    str: 5,
+                    dex: 4,
+                    int: 10
+                }
+            },
+            male: {
+                gandalf: {
+                    life: 100,
+                    mana: 100,
+                    atk: 8,
+                    def: 5,
+                    str: 5,
+                    dex: 5,
+                    int: 10
+                },
+                dumbledore: {
+                    life: 100,
+                    mana: 100,
+                    atk: 7,
+                    def: 3,
+                    str: 4,
+                    dex: 4,
+                    int: 10
+                },
+                medivh: {
+                    life: 100,
+                    mana: 100,
+                    atk: 6,
+                    def: 4,
+                    str: 4,
+                    dex: 3,
+                    int: 9
+                },
+                cain: {
+                    life: 100,
+                    mana: 100,
+                    atk: 4,
+                    def: 3,
+                    str: 3,
+                    dex: 2,
+                    int: 9
+                }
+            }
+        }
+    }
+;
+
+/**
+ * 
+ * @param {String} sexe 
+ * @param {String} job 
+ * @param {String} name 
+ * 
+ * @return {Object/Null} Propriétés d'un personnage
+ */
+const getCharacterProperties = (sexe, job, name) => {
+    
+    if(characters.hasOwnProperty(job)){
+        if(characters[job].hasOwnProperty(sexe)){
+            if(characters[job][sexe].hasOwnProperty(name)){
+                return characters[job][sexe][name]
+            }
+        }
+    }
+
+    return 
+}
+
+/**
+ * 
+ * @param {String} sexe 
+ * @param {String} job
+ * 
+ * @return {Array/Null} Liste de personnage en fonction du sexe et de la classe
+ */
+const getCharacters = (sexe, job) => {
+    if(characters.hasOwnProperty(job)){
+        if(characters[job].hasOwnProperty(sexe))
+            return Object.keys(characters[job][sexe])
+    }
+
+    return 
+}
+
+export { getCharacterProperties, getCharacters }
---
+++ test/test.js
@@ -0,0 +1,36 @@
+import { getCharacterProperties, getCharacters } from '../Characters'
+import assert from 'assert'
+
+describe('Characters', function(){
+    describe('getCharacterProperties', function(){
+        it('Doit retourner les caractéristiques du personnage', function(){
+            const charac = getCharacterProperties('female','berserk', 'clare')
+            const expected = {
+                life: 100,
+                mana: 100,
+                atk: 10,
+                def: 5,
+                str: 10,
+                dex: 3,
+                int: 10
+            }
+            assert.deepEqual(charac, expected)
+        })
+
+        it('Doit retourner null si les paramètres ne sont pas correctes', function(){
+            const charac = getCharacterProperties('fdsf', 1, 100)
+            assert.deepEqual(charac, null)
+        })
+    })
+    describe('getCharacters()', function(){
+        it('Doit retourner un tableau de personnages', function(){
+            const characs = getCharacters('female', 'berserk')
+            const expected = ['dolores', 'clare', 'ripley', 'garona']
+            assert.deepEqual(characs, expected)
+        })
+        it('Doit retourner null si les paramètres ne sont pas correctes', function(){
+            const characs = getCharacters('fds',1)
+            assert.deepEqual(characs, null)
+        })
+    })
+})
